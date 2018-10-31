fd169c51b6246df870d718bd088433f93691fc1c
sully34 <sssulai@gmail.com>
2018-10-31 15:54:30 -0400

    Updates

---
+++ precache-manifest.0acc327891d578b35388ed82c2e2089f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd69dee79a2c047dea0e68d1add02ad2",
+    "url": "/Sulai3030/static/media/3.bd69dee7.jpg"
+  },
+  {
+    "revision": "11deed1e9c01d3129f27",
+    "url": "/Sulai3030/static/js/runtime~main.11deed1e.js"
+  },
+  {
+    "revision": "7df16847204155f20955",
+    "url": "/Sulai3030/static/js/main.7df16847.chunk.js"
+  },
+  {
+    "revision": "88e2bac6674bbcd4eb29",
+    "url": "/Sulai3030/static/js/1.88e2bac6.chunk.js"
+  },
+  {
+    "revision": "7df16847204155f20955",
+    "url": "/Sulai3030/static/css/main.c298406e.chunk.css"
+  },
+  {
+    "revision": "f636bae6943361fc92f6d7119979cc89",
+    "url": "/Sulai3030/index.html"
+  }
+];
