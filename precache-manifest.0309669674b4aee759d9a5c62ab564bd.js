a60e47fdfe642da43b3ce6311c977c481dd41e0c
sully34 <sssulai@gmail.com>
2018-11-10 12:30:23 -0500

    Updates

---
+++ precache-manifest.0309669674b4aee759d9a5c62ab564bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd69dee79a2c047dea0e68d1add02ad2",
+    "url": "/clickgame/static/media/3.bd69dee7.jpg"
+  },
+  {
+    "revision": "1d1da0236ccd610ca49c",
+    "url": "/clickgame/static/js/runtime~main.1d1da023.js"
+  },
+  {
+    "revision": "a4f1a97225b0bf634a08",
+    "url": "/clickgame/static/js/main.a4f1a972.chunk.js"
+  },
+  {
+    "revision": "1162a8c105d10c526101",
+    "url": "/clickgame/static/js/1.1162a8c1.chunk.js"
+  },
+  {
+    "revision": "a4f1a97225b0bf634a08",
+    "url": "/clickgame/static/css/main.766c7513.chunk.css"
+  },
+  {
+    "revision": "4af41339fa11be115e455284dbc20fdb",
+    "url": "/clickgame/index.html"
+  }
+];
